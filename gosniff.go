0623d0637b7b7fb4a368fa5c99b20165cc5a60ca
Fernandez Chris <chris.fernandez@bugcrowd.com>
2017-01-16 18:37:01 -0800

    added the promiscuous mode switch and prepared the dump to pcap file base

--- gosniff.go
+++ gosniff.go
@@ -19,10 +19,10 @@ var (
 	err          error
 	timeout      time.Duration = -1 * time.Second
 	handle       *pcap.Handle
+	sniffdump    string
 )
 
 func main() {
-
 	app := &cli.App{
 		Name:      "gosniff",
 		Version:   "0.0.1",
@@ -36,12 +36,28 @@ func main() {
 
 			&cli.StringFlag{
 				Name:        "interface",
+				Aliases:     []string{"i"},
 				Value:       "eth0",
 				Usage:       "the interface to use",
 				Destination: &device,
 			},
 			&cli.StringFlag{
+				Name:    "file",
+				Aliases: []string{"f"},
+				//Value:       "sniffdump",
+				Usage:       "To log create a pcap with sniff dump",
+				Destination: &sniffdump,
+			},
+			&cli.BoolFlag{
+				Name:    "promiscuous",
+				Aliases: []string{"p"},
+				//Value:       "false",
+				Usage:       "To enable promiscuous mode",
+				Destination: &promiscuous,
+			},
+			&cli.StringFlag{
 				Name:        "sniff",
+				Aliases:     []string{"s"},
 				Value:       "tcp and port 443",
 				Usage:       "the BPF syntax parameters to sniff on",
 				Destination: &suck,
@@ -49,7 +65,8 @@ func main() {
 		},
 		Action: func(c *cli.Context) error {
 			fmt.Printf("Capturing on Interface %v\n", c.String("interface"))
-			sniff(device, suck)
+			fmt.Printf("Promiscuous mode: %v\n", c.String("promiscuous"))
+			sniff(device, suck, sniffdump)
 			return nil
 		},
 		Commands: []*cli.Command{
@@ -69,7 +86,7 @@ func main() {
 	app.Run(os.Args)
 }
 
-func sniff(device string, suck string) string {
+func sniff(device string, suck string, sniffdump string) string {
 
 	// Open device
 	handle, err = pcap.OpenLive(device, snapshot_len, promiscuous, timeout)
@@ -89,6 +106,7 @@ func sniff(device string, suck string) string {
 	packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
 	for packet := range packetSource.Packets() {
 		fmt.Println(packet)
+		//if capture to pcap file enable then file_cap(packet) etc
 	}
 	return device
 }
@@ -100,4 +118,7 @@ func list_int() {
 		log.Fatal(err)
 	}
 	fmt.Println(devices)
+}
+
+func file_cap() {
 }
